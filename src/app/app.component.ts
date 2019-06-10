98c6f87dceaefa0c4afa1a7a9d1aac1cf3d15a57
Sam Le <sam.le@liu.se>
2019-06-10 09:14:59 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // tslint:disable-next-line:component-selector
+  selector: 'app',
+  template: '<store></store>'
 })
 export class AppComponent {
   title = 'SportsStore';
